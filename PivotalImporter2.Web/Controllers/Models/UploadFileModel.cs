76607b1168a71e439ed43c18a7af887fc7873689
petemckee <p15tol@yahoo.com>
2013-06-24 20:49:47 +0100

    suo added to ignore. UploadFileModel.css added

---
+++ PivotalImporter2.Web/Controllers/Models/UploadFileModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace PivotalImporter2.Web.Controllers.Models
+{
+	public class UploadFileModel
+	{
+		[Required]
+		public int ProjectId { get; set; }
+
+		// TODO! Add file type check to validation..?
+		[Required]
+		public HttpPostedFileBase File { get; set; }
+
+		public UploadFileModel()
+		{
+		}
+
+		public UploadFileModel(int projectId, HttpPostedFileBase file)
+		{
+			ProjectId = projectId;
+			File = file;
+		}
+	}
+}
