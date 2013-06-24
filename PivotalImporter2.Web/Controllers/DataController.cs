732c21c5ae65273240a6610fa1dc9d131b21abd8
petemckee <p15tol@yahoo.com>
2013-06-24 21:22:48 +0100

    DataController.cs added

---
+++ PivotalImporter2.Web/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using PivotalImporter2.Domain.Services;
+
+namespace PivotalImporter2.Web.Controllers
+{
+	// TODO! Use APIController - and IDependencyResolver (StructureMap.MVC4? from Nuget...)
+	[HandleError]
+	public class DataController : Controller
+    {
+	    private readonly IPivotalService pivotalService;
+
+	    public DataController(IPivotalService pivotalService)
+	    {
+		    this.pivotalService = pivotalService;
+	    }
+
+	    public ActionResult ProjectUsers(int projectId)
+		{
+			var memberships = pivotalService.ProjectUsers(projectId);
+		    return Json(memberships, JsonRequestBehavior.AllowGet);
+		}
+    }
+}
